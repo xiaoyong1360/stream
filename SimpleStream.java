390f2c160738750715a76b50ce45278d010546cd
Z L <lizhaoshi@gmail.com>
2025-07-16 09:57:35 -0400

    double

--- SimpleStream.java
+++ SimpleStream.java
@@ -3,37 +3,27 @@ import org.w3c.dom.ls.LSOutput;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.OptionalDouble;
 
 public class SimpleStream {
-    public static void main2(String[] args) {
-        List<Integer> li = List.of(6 ,4 ,3, 2, 6, 9 ,1 ,2);
-//        List<Integer> li = new ArrayList<>();// List.of(6 ,4 ,3, 2, 6, 9 ,1 ,2);
-        Optional<Integer> max = li.stream().max(Integer::compareTo);
-        if(max.isEmpty()){
+    public static void main(String[] args) {
+        List<Integer> li = List.of(6, 4, 3, 2, 6, 9, 1, 2);
+
+        OptionalDouble average = li.stream().mapToDouble(Integer::doubleValue).average();
+        Optional<Double> ad = Optional.ofNullable(null);
+        System.out.println(ad.isPresent() );
+
+        double t;
+        if (average.isPresent()) {
+            t = average.getAsDouble();
+            System.out.println(t);
+        } else {
             System.out.println("empty");
         }
-        else {
-            System.out.println(max.get());
-        }
-        Integer integer = li.stream().max(Integer::compareTo).orElse(0);
-        System.out.println(integer);
+
+
+
 
     }
-    public static void main(String[] args) {
-        Optional<String> optional1 = Optional.of("hello");
-        Optional<String> optional2 = Optional.empty();
-        Optional<String> optional3 = Optional.ofNullable(null);
-//        Optional<String> optional4 = Optional.of(null);
 
-//        System.out.println(optional3.isPresent());
-        optional3.ifPresentOrElse(System.out::print, () -> System.out.println("empty") );
-
-        
-
-
-//        System.out.println("optional1 isPresent: " + optional1.isPresent()); // true
-//        System.out.println("optional1 isEmpty: " + optional1.isEmpty());   // false
-//
-//        System.out.println("optional2 isPresent: " + optional2.isPresent()); // false
-//        System.out.println("optional2 isEmpty: " + optional2.isEmpty());   // true
-    }}
+}
